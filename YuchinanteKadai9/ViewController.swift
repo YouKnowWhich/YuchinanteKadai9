256f16fdb4f157fcc8cc260d6c2751fda32c6eb6
yuya-3110 <ysuu45143745@gmail.com>
2024-02-26 23:54:38 +0900

    Initial Commit

---
+++ YuchinanteKadai9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by Yuchinante on 2024/02/26
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
